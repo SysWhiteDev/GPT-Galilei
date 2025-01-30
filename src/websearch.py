edb04648c8a0a0cd4a5903461cc614adf8ee5f97
SysWhiteDev <syswhite.commercial@gmail.com>
2025-01-30 10:05:56 +0100

    Updated ammount of indexed pages from 3 to 2 CoAuthor: LalloS

--- src/websearch.py
+++ src/websearch.py
@@ -4,7 +4,7 @@ import googlesearch
 QUERY_FORMAT = "I will give you a question. I want a short google query using only keywords and words from the question: {q}"
 
 
-def search(query: str, n_results: int = 3) -> list[str]:
+def search(query: str, n_results: int = 2) -> list[str]:
     # ignore duplicated links
     return list(set(googlesearch.search(query, num_results=n_results))) # type: ignore
 
